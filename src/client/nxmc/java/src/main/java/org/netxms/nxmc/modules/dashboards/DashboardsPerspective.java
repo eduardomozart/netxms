3bd78082e6f2d576df0c3e9f69f77563e1f02e4d
E <2974895+eduardomozart@users.noreply.github.com>
2026-05-02 14:50:13 -0300

    Use LocalizationHelper.getI18n in perspectives
    Replace global i18n.tr(...) calls with LocalizationHelper.getI18n(<Class>).tr(...) in multiple perspective classes (Assets, BusinessServices, Dashboards, Graphs, Infrastructure, Maps, Network, Templates). This enables class-scoped localization/resource bundle resolution for each perspective.

--- src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/dashboards/DashboardsPerspective.java
+++ src/client/nxmc/java/src/main/java/org/netxms/nxmc/modules/dashboards/DashboardsPerspective.java
@@ -33,7 +33,7 @@ public class DashboardsPerspective extends ObjectsPerspective
 
    public DashboardsPerspective()
    {
-      super("objects.dashboards", i18n.tr("Dashboards"), "icons/perspectives/dashboards.svg", SubtreeType.DASHBOARDS, null);
+      super("objects.dashboards", LocalizationHelper.getI18n(DashboardsPerspective.class).tr("Dashboards"), "icons/perspectives/dashboards.svg", SubtreeType.DASHBOARDS, null);
    }
 
    /**
