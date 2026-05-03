e490e496becf792ab3585016ba542f56d2215078
E <2974895+eduardomozart@users.noreply.github.com>
2026-05-03 00:23:02 -0300

    Revert "Initialize Registry state dir earlier"
    This reverts commit ce1fde26db08f3f53f28233dcd6ebe1b88f798f7.

--- src/client/nxmc/java/src/rwt/java/org/netxms/nxmc/Startup.java
+++ src/client/nxmc/java/src/rwt/java/org/netxms/nxmc/Startup.java
@@ -138,7 +138,6 @@ public class Startup implements EntryPoint, StartupParameters
       {
          stateDir.mkdir();
       }
-      Registry.setStateDir(stateDir);
       logger.info("NetXMS Management Console version " + VersionInfo.version() + " starting");
       logger.info("State directory: " + stateDir.getAbsolutePath());
 
@@ -157,6 +156,8 @@ public class Startup implements EntryPoint, StartupParameters
          language = PreferenceStore.getInstance().getAsString("nxmc.language", "en");
       logger.info("Language: " + language);
       RWT.setLocale(LocalizationHelper.localeFromLanguageCode(language));
+
+      Registry.setStateDir(stateDir);
 
       DateFormatFactory.createInstance();
       SharedIcons.init();
